0fcded83a9a3d5804984d594842bf45f8dc2f94a
Saimoen <gregorysaimoen@outlook.com>
2024-02-15 10:19:51 +1100

    AVdJ trois + ajout fichier scss

--- src/Controller/MarquePageController.php
+++ src/Controller/MarquePageController.php
@@ -8,30 +8,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 
+#[Route("/marquepage", requirements: ["_locale" => "en|es|fr"], name: "marquepage_")]
 class MarquePageController extends AbstractController
 {
-    // Définition de la route 
-    #[Route('/')]
-    public function getAll(EntityManagerInterface $entityManager): Response
-    {
-        // Récupère les livres dans la BDD
-        $marquePage = $entityManager->getRepository(MarquePage::class)->findAll();
-        
-        // Gestion d'erreur
-        if(!$marquePage) {
-            throw $this->createNotFoundException("Aucun livre n'est enregistré !");
-        }
-
-        // Transfère les données à la Vue
-        return $this->render('liste/liste.html.twig', [
-            'marquePage' => $marquePage,
-        ]);
-    }
-
         // Définition de la route 
-        #[Route('/marquepage/ajouter')]
+        #[Route('/ajouter')]
         public function addMarquePage(EntityManagerInterface $entityManager): Response
         {
             // Instanciation de la classe MarquePage
@@ -45,6 +27,25 @@ class MarquePageController extends AbstractController
             $marquePage = $entityManager->persist($marquePage);
             $entityManager->flush();
 
-            return new Response("<a href='/'>Retourner</a>");
+            return new Response("<h1>Marque Page ajouté</h1><a href='/'>Retourner</a>");
+        }
+
+        // Définition de la route 
+        #[Route('/consulter/{id}', requirements: ["id" => "\d+"])]
+        public function consulterDetails(int $id, EntityManagerInterface $entityManager): Response
+        {
+            // Cherche les marques pages en bdd selon l'ID de l'article
+            $details = $entityManager->getRepository(MarquePage::class)->find($id);
+
+            if(!$details) {
+                throw $this->createNotFoundException(
+                    "Aucun marque page avec l'id ". $id
+                );
+            }
+
+            // Ajoute les valeurs dans la Vue
+            return $this->render('details/details.html.twig', [
+                'details' => $details,
+            ]);
         }
 }
